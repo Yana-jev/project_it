41f205fc281064e43381eb004095e40de63ac778
Yana-jev <evko.yana@gmail.com>
2024-11-11 13:31:47 +0100

    add calendar

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,25 +1,53 @@
 import { Component, inject } from '@angular/core';
 import { EventService } from '../../data/services/event.service';
-import { JsonPipe } from '@angular/common';
+import { CommonModule} from '@angular/common';
 import { iEvent } from '../../data/services/interfaces/ievents';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'events',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [CommonModule, FormsModule],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.scss'
+  styleUrl: './events.component.scss',
+
 })
 export class EventsComponent {
 
-  eventService = inject(EventService)
-  events: iEvent[] = []
+  eventService = inject(EventService);
+  events: iEvent[] = []; 
+  filteredEvents: iEvent[] = []; 
+  startDate: string = ''; 
+  endDate: string = ''; 
 
-  constructor(){
+  constructor() {
+
     this.eventService.getTestEvent()
-    .subscribe(val =>{
-      console.log('Received events:', val);
-      this.events = val
-    })
+      .subscribe(val => {
+        console.log('Received events:', val);
+        this.events = val;
+        this.filteredEvents = val; 
+      });
   }
+
+
+  filterEvents() {
+    if (this.startDate && this.endDate) {
+
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+
+      this.filteredEvents = this.events.filter(event => {
+        const eventDate = new Date(event.date);
+        return eventDate >= start && eventDate <= end;
+      });
+    } else {
+
+      this.filteredEvents = this.events;
+    }
+    console.log('Filtered Events:', this.filteredEvents);
+  }
+
 }
+
+
