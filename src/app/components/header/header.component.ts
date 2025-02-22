508e0a2a897c95ef2b11c01e4656da31e559b63e
Yana-jev <evko.yana@gmail.com>
2025-02-22 14:26:06 +0100

    add styles header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,43 +7,14 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  authService = inject(AuthService);
-  router = inject(Router);
-  itemCount = signal(0);
+
   isMenuOpen = false;
+  constructor() { }
 
-  constructor(private cartService: CartService) {
-    this.updateItemCount();
-
-    this.cartService.cartUpdates$.subscribe(() => {
-      this.updateItemCount(); 
-    });
-  }
-
-
-  private updateItemCount(): void {
-    this.cartService.getTotalItems().subscribe((totalItems) => {
-      this.itemCount.set(totalItems);
-    });
-  }
-
-
-  onLogout() {
-    this.authService.logout().subscribe(
-      () => {
-        console.log('Logout successful');
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        console.error('Logout failed', error);
-        alert('Logout failed: ' + error.error.message || 'Unknown error');
-      }
-    );
-  }
 }
 
