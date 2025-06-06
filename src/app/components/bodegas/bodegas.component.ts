f9e929a918b2285281e42afe5aa5732ba3a519a9
Yana-jev <evko.yana@gmail.com>
2025-06-06 11:15:17 +0200

    add translate module

--- src/app/components/bodegas/bodegas.component.ts
+++ src/app/components/bodegas/bodegas.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Pipe } from '@angular/core';
 import { Router } from '@angular/router';
 import { iBodega } from '../../data/services/interfaces/ibodega';
 import { BodegaService } from '../../data/services/bodega.service';
 import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'bodegas',
@@ -18,8 +20,11 @@ export class BodegasComponent {
   startDate: string = ''; 
   endDate: string = ''; 
   
+color = 'blue'
+ageOfUsers = [{age: 20}, {age: 26}, {age: 22}]
 
   constructor(private router: Router) {
+
 
     this.bodegaService.getBodega()
       .subscribe(val => {
@@ -28,6 +33,14 @@ export class BodegasComponent {
       });
   }
 
+  changeColor(newColor: string){
+    this.color = newColor
+  }
+
+  checkCurrentValue(event: Event){
+    const target = event.target as HTMLInputElement
+console.log('e', target.value)
+  }
   goToBodegaDetail(bodegaId: number) {
 
     this.router.navigate([`/bodega-detail/${bodegaId}`]);  
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,18 +3,31 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../data/services/auth.service';
 import { CartService } from '../../data/services/cart.service';
 import { CommonModule } from '@angular/common';
+import {TranslateModule, TranslateService} from '@ngx-translate/core'
+
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink],
+  imports: [RouterOutlet, CommonModule, RouterLink, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   isMenuOpen = false;
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    translate.addLangs([ 'es', 'en', 'ru' ]);
+    translate.setDefaultLang ('es')
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/en|ru|es/) ? browserLang : 'en');
+  }
+
+  switchLang(event: Event){
+    const lang = (event.target as HTMLSelectElement).value;
+    this.translate.use(lang)
+   
+  }
 
 }
 
