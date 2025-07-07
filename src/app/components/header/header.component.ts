d858372bb4f57193c78c0afb53185d20408ce46d
Yana-jev <evko.yana@gmail.com>
2025-07-07 19:55:09 +0200

    add lenguages header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,31 +3,38 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../data/services/auth.service';
 import { CartService } from '../../data/services/cart.service';
 import { CommonModule } from '@angular/common';
-import {TranslateModule, TranslateService} from '@ngx-translate/core'
+import {TranslateService,TranslatePipe,TranslateDirective} from '@ngx-translate/core'
 
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, TranslateModule],
+  imports: [RouterOutlet, CommonModule, RouterLink, TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+ isMenuOpen = false;
+  currentLang = 'es';
+  isLoaded = false;
+ constructor(private translate: TranslateService) {
+    translate.addLangs(['es', 'en', 'ru']);
+    const savedLang = localStorage.getItem('lang') || 'es';
+    this.currentLang = savedLang;
+    translate.setDefaultLang(savedLang);
+    translate.use(savedLang);
 
-  isMenuOpen = false;
-  constructor(private translate: TranslateService) {
-    translate.addLangs([ 'es', 'en', 'ru' ]);
-    translate.setDefaultLang ('es')
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang?.match(/en|ru|es/) ? browserLang : 'en');
+    // ⏳ Подписка на событие завершения загрузки языка
+    this.translate.onLangChange.subscribe(() => {
+      this.isLoaded = true;
+    });
   }
 
-  switchLang(event: Event){
+  switchLang(event: Event) {
     const lang = (event.target as HTMLSelectElement).value;
-    this.translate.use(lang)
-   
+    this.isLoaded = false; // сбрасываем флаг, ждём загрузки
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+    this.currentLang = lang;
   }
-
 }
-
